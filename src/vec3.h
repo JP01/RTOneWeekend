6d2bd01bef096bae4e0a79fca8251eaeae7b6b8f
JP01 <jpmackel01@gmail.com>
2022-07-20 18:12:50 -0700

    Added metal materials. Create a render of some metal balls.

--- src/vec3.h
+++ src/vec3.h
@@ -52,6 +52,12 @@ public:
     inline static vec3 random(double min, double max) {
         return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
     }
+
+    bool near_zero() const {
+        // Return true if the vector is close to zero in all dimensions.
+        const auto s = 1e-8;
+        return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+    }
 public:
     double e[3];
 };
@@ -59,6 +65,7 @@ public:
 // Type aliases for vec3
 using point3 = vec3;   // 3D point
 using color = vec3;    // RGB color
+
 
 // vec3 Utility Functions
 
@@ -126,4 +133,7 @@ vec3 random_in_hemisphere(const vec3& normal) {
         return -in_unit_sphere;
 }
 
+vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2 * dot(v, n) * n;
+}
 #endif
